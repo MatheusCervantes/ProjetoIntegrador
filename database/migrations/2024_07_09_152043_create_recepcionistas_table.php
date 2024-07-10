6b3bf75432466c80ce137ff4afb26ba9b17a1bd2
MatheusCervantes <matheuscervantes1@gmail.com>
2024-07-10 17:39:51 -0300

    Insert secretaria e usuario

---
+++ database/migrations/2024_07_09_152043_create_recepcionistas_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('recepcionistas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('nome_completo');
+            $table->string('sexo');
+            $table->string('cpf')->unique();
+            $table->string('rg')->unique();
+            $table->date('data_nascimento');
+            $table->string('email')->unique();
+            $table->string('telefone');
+            $table->string('rua');
+            $table->integer('numero');
+            $table->string('complemento')->nullable();
+            $table->string('cidade');
+            $table->string('estado');
+            $table->string('cep');
+            $table->string('foto')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('recepcionistas');
+    }
+};
---
+++ resources/views/emails/usuario.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Informações de Login</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+        <div>
+            <div class="d-flex justify-content-center">
+                <h2 class="text-center">{{ $data['nome'] }}, aqui estão suas informações de login:</h2>
+            </div>
+            <br>
+            <p><b>USUÁRIO: </b>{{ $data['username'] }}</p>
+            <p><b>SENHA: </b> {{ $data['password'] }}</p>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
